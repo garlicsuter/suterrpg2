ab46af9719acf78452b43c1121c737884f3219fe
garlicsuter <markasuter@gmail.com>
2019-10-28 08:32:56 -0400

    fixed CharInventory - crate inactive

---
+++ RPG-Suter/Assets/Scripts/CharInventory2.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharInventory2 : MonoBehaviour
+{
+    public int needItCount = 0;
+    public GameObject crate;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //when player collides with objects tagged "needit",
+        // then change a variable to +1.
+        // if that +1 makes the total 2, turn of crate.
+
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("needit"))
+        {
+            needItCount += 1;
+            Debug.Log("you have an apple.");
+            if(needItCount >= 2)
+            {
+                crate.SetActive(false);
+            }
+        }
+    }
+
+}
