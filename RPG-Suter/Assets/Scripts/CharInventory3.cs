5ed7d534d3e7f21363128feff908823be5c2a13e
garlicsuter <markasuter@gmail.com>
2019-10-29 10:12:04 -0400

    new char inventory script

---
+++ RPG-Suter/Assets/Scripts/CharInventory3.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharInventory3 : MonoBehaviour
+{
+    //Declare Variables
+    public int needItCount = 0;
+    public GameObject crate;
+
+    //When we collide with a trigger
+    //if the other thing is tagged "needit"
+    //then add 1 to our needItCount
+    //and if needItCount is > x, crate.Setactive(false)
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("needit"))
+        {
+            Debug.Log("you got an apple!");
+            needItCount += 1;
+            if(needItCount >= 2)
+            {
+                crate.SetActive(false);
+            }
+        }
+    }
+}
